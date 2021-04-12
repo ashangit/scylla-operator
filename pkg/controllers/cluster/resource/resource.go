f9400127a20ffcf28e158ac48a7cacab6eb5d6ce
Nicolas Fraison <nfraison@yahoo.fr>
2021-04-12 22:00:05 +0200

    Remove bad comment

--- pkg/controllers/cluster/resource/resource.go
+++ pkg/controllers/cluster/resource/resource.go
@@ -52,7 +52,6 @@ func HeadlessServiceForCluster(c *scyllav1.ScyllaCluster) *corev1.Service {
 }
 
 func MemberServiceForPod(pod *corev1.Pod, cluster *scyllav1.ScyllaCluster) *corev1.Service {
-	// TODO use NodePort if hostNetwork?
 	labels := naming.ClusterLabels(cluster)
 	labels[naming.DatacenterNameLabel] = pod.Labels[naming.DatacenterNameLabel]
 	labels[naming.RackNameLabel] = pod.Labels[naming.RackNameLabel]
